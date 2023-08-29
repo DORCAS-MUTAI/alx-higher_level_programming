835c8b0eb5eb3c7b3b677998c48481e07228df81
DORCAS-MUTAI <dorcasjmutai@gmail.com>
2023-08-28 22:48:26 -0700

    üpdate

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,7 +9,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except ZeroDivisionError:
+        except Exception:
             result = b + a
             break
     return (result)
